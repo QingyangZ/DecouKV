b1f06f8dd5effe8e8b6419b5a862e9e480dca2f3
BorgesLu <lmx153134150@icloud.com>
2025-05-24 12:56:44 +0800

    Modify test.sh

---
+++ db_impl/matrixkv/util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:17c5eeb3ea5d8e768344df6c885c8238c2d1a78f";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2025-05-24";
+const char* rocksdb_build_compile_date = __DATE__;
